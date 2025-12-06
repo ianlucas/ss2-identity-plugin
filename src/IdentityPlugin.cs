0917a7b763942e021eedfa4d0e21f281af402126
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-12-06 15:52:10 -0300

    Use primary constructor

--- src/IdentityPlugin.cs
+++ src/IdentityPlugin.cs
@@ -17,23 +17,28 @@ namespace IdentityPlugin;
     Author = "Ian Lucas",
     Description = "Authenticates external users."
 )]
-public partial class IdentityPlugin : BasePlugin
+public partial class IdentityPlugin(ISwiftlyCore core) : BasePlugin(core)
 {
-    public readonly IConVar<string> Url;
-    public readonly IConVar<bool> IsStrict;
-    public readonly IConVar<bool> IsForceNickname;
-    public readonly IConVar<bool> IsForceRating;
-
-    public IdentityPlugin(ISwiftlyCore core)
-        : base(core)
-    {
-        // csharpier-ignore-start
-        Url             = core.ConVar.Create("identity_url",            "URL for fetching player identity.",                        "");
-        IsStrict        = core.ConVar.Create("identity_strict",         "Whether to kick the player if we fail to get their data.", true);
-        IsForceNickname = core.ConVar.Create("identity_force_nickname", "Whether to force player nickname.",                        true);
-        IsForceRating   = core.ConVar.Create("identity_force_rating",   "Whether to force player rating.",                          true);
-        // csharpier-ignore-end
-    }
+    public readonly IConVar<string> Url = core.ConVar.Create(
+        "identity_url",
+        "URL for fetching player identity.",
+        ""
+    );
+    public readonly IConVar<bool> IsStrict = core.ConVar.Create(
+        "identity_strict",
+        "Whether to kick the player if we fail to get their data.",
+        true
+    );
+    public readonly IConVar<bool> IsForceNickname = core.ConVar.Create(
+        "identity_force_nickname",
+        "Whether to force player nickname.",
+        true
+    );
+    public readonly IConVar<bool> IsForceRating = core.ConVar.Create(
+        "identity_force_rating",
+        "Whether to force player rating.",
+        true
+    );
 
     public override void Load(bool hotReload)
     {
