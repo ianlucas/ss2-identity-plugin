1f480f220ea729f3f63316924f2c8ce7b4c3788b
Ian Lucas <ianlucas_ferreira@hotmail.com>
2026-02-07 16:25:51 -0300

    Remove unused module

--- src/Identity.Hooks.cs
+++ src/Identity.Hooks.cs
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-using System.Runtime.InteropServices;
 using SwiftlyS2.Shared.SchemaDefinitions;
 
 namespace Identity;
